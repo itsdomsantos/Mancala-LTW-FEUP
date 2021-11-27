e560bb0df935eab49dd3a75a91bdc3d879398c3c
Andre Costa <up201905916@fe.up.pt>
2021-11-27 16:37:53 +0000

    pop-ups a funcionar, falta a dúvida das cavidades

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
-const draggables = document.querySelectorAll('.draggable') /* Sementes */
-const containers = document.querySelectorAll('.cavidades, .cavidades2, .armazem1, .armazem2') /* Depósito de sementes */
+const modalButtons = document.querySelectorAll('.modal-show, .modal-close'); // Get all buttons used for showing and hiding the modals
 
+modalButtons.forEach(button => { // atach a click event listener to each button and pass it a named callback function
+  button.addEventListener('click', showOrHideModal);
+});
 
-/* Loop que trata do Drag */
-draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('dragging')
-    })
-    draggable.addEventListener('dragend', () => {
-        draggable.classList.remove('dragging')
-    })
-})
-
-/* Loop que trata do Drop */
-containers.forEach(container => {
-    container.addEventListener('dragover', e => {
-        e.preventDefault()
-        const draggable = document.querySelector('.dragging')
-        container.appendChild(draggable)
-    })
-
-})
+function showOrHideModal(evt){
+	const currentButtonElement = evt.target;
+  if(currentButtonElement.dataset.modalId) { // if the button has the data attribute modalId, it's a 'show' button
+  	const targetModalId = currentButtonElement.dataset.modalId // get the target modal id from the data attribute
+  	const targetModalElement = document.getElementById(targetModalId); // get the target modal element
+    targetModalElement.classList.remove('modal-hidden'); // remove the CSS class used to hide it
+  } else { // the button is a 'close' button
+  	const parentModal = currentButtonElement.parentElement; // get the parent modal element
+    parentModal.classList.add('modal-hidden'); // add the CSS class used to hide it
+  }
+  
+}
 
