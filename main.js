728751031b4683878b42c3a931b2830d883c3828
Domingos Santos <domingosjmsantos@gmail.com>
2021-11-29 17:55:26 +0000

    added beans

--- main.js
+++ main.js
@@ -27,3 +27,23 @@ output.innerHTML = slider.value; // Display the default slider value
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+/*
+const cavidades_total = document.querySelectorAll(".tabuleiro"); // Get all cavidades incluindo os dois armazens, e as cavidades top e bottom
+
+cavidades_total.forEach(cavidade => { // atach a click event listener to each button and pass it a named callback function
+  cavidade.addEventListener('click', moveSeeds);
+});
+
+function moveSeeds(evt){
+	const currentCavity = evt.target;
+  if(currentButtonElement.dataset.modalId) { // if the button has the data attribute modalId, it's a 'show' button
+  	const targetModalId = currentButtonElement.dataset.modalId // get the target modal id from the data attribute
+  	const targetModalElement = document.getElementById(targetModalId); // get the target modal element
+      
+    targetModalElement.classList.remove('modal-hidden'); // remove the CSS class used to hide it
+  } else { // the button is a 'close' button
+  	const parentModal = currentButtonElement.parentElement; // get the parent modal element
+    parentModal.classList.add('modal-hidden'); // add the CSS class used to hide it
+  }
+  
+}*/
