c33fd1c11154f9ba6ad0d8c79286d3e8e9bb8a0c
Andre Costa <up201905916@fe.up.pt>
2021-12-02 18:44:33 +0000

    output error resolved

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function showOrHideModal(evt){
 
 
 var slider = document.getElementById("myRange");
-var output;
+var output = document.getElementById("output");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
